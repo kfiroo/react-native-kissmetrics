e7b413b87b7bd3cb4a5cba44afb710a0c5a9ac4d
kfiroo <kfirgolan@gmail.com>
2016-12-28 15:13:05 +0200

    ios

---
+++ ios/RNKissmetrics/RNKissmetrics/RNKissmetrics.h
@@ -0,0 +1,13 @@
+//
+//  RNKissmetrics.h
+//  RNKissmetrics
+//
+//  Created by Kfir Golan on 28/12/2016.
+//  Copyright © 2016 Kfir Golan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNKissmetrics : NSObject
+
+@end
---
+++ ios/RNKissmetrics/RNKissmetrics/RNKissmetrics.m
@@ -0,0 +1,13 @@
+//
+//  RNKissmetrics.m
+//  RNKissmetrics
+//
+//  Created by Kfir Golan on 28/12/2016.
+//  Copyright © 2016 Kfir Golan. All rights reserved.
+//
+
+#import "RNKissmetrics.h"
+
+@implementation RNKissmetrics
+
+@end
