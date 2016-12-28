ad989c50f1233de6ac39eec579537c18ac175e33
kfiroo <kfirgolan@gmail.com>
2016-12-28 17:19:32 +0200

    fix identity decleration

--- ios/RNKissmetrics/RNKissmetrics/RNKissmetrics.m
+++ ios/RNKissmetrics/RNKissmetrics/RNKissmetrics.m
@@ -9,7 +9,6 @@
 #import "RNKissmetrics.h"
 
 #import "RCTLog.h"
-#import "RCTBridge.h"
 #import "RCTConvert.h"
 #import "RCTUtils.h"
 
@@ -26,7 +25,7 @@ RCT_EXPORT_METHOD(log:(NSString *)message)
 
 RCT_EXPORT_METHOD(configure:(NSDictionary *)config)
 {
-//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    //    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
     NSString *key = [RCTConvert NSString:config[@"apiKey"]];
     [KISSmetricsAPI sharedAPIWithKey:key];
     if ([RCTConvert BOOL:config[@"autoRecordInstalls"]]) {
@@ -43,14 +42,15 @@ RCT_EXPORT_METHOD(configure:(NSDictionary *)config)
 // identify
 RCT_EXPORT_METHOD(identify:(NSString *)identity)
 {
-//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    //    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
     [[KISSmetricsAPI sharedAPI] identify:identity];
 }
 
 // identity
-RCT_EXPORT_METHOD(identity resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(identity:(RCTPromiseResolveBlock)resolve
+                  rejecter:(__unused RCTPromiseRejectBlock)reject)
 {
-//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    //    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
     NSString *currentIdentity = [[KISSmetricsAPI sharedAPI] identity];
     resolve(currentIdentity);
 }
@@ -58,22 +58,21 @@ RCT_EXPORT_METHOD(identity resolver:(RCTPromiseResolveBlock)resolve rejecter:(RC
 // clearIdentity
 RCT_EXPORT_METHOD(clearIdentity)
 {
-//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
-    [[KISSmetricsAPI sharedAPI] clearIdentity]
+    //    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    [[KISSmetricsAPI sharedAPI] clearIdentity];
 }
-
 
 // alias
 RCT_EXPORT_METHOD(alias:(NSString *)alias withIdentity:(NSString *)identity)
 {
-//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    //    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
     [[KISSmetricsAPI sharedAPI] alias:alias withIdentity:identity];
 }
 
 // record
 RCT_EXPORT_METHOD(record:(NSString *)eventName withProperties:(NSDictionary *)properties)
 {
-//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    //    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
     [[KISSmetricsAPI sharedAPI] record:eventName withProperties:properties];
 }
 
@@ -81,7 +80,7 @@ RCT_EXPORT_METHOD(record:(NSString *)eventName withProperties:(NSDictionary *)pr
 // set
 RCT_EXPORT_METHOD(set:(NSDictionary *)properties)
 {
-//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    //    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
     [[KISSmetricsAPI sharedAPI] set:properties];
 }
 
