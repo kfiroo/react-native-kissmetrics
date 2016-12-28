48f5bea96990d75175837dbd6f769831edd53c7f
kfiroo <kfirgolan@gmail.com>
2016-12-28 16:56:29 +0200

    impl RNKissmetrics.m

--- ios/RNKissmetrics/RNKissmetrics/RNKissmetrics.m
+++ ios/RNKissmetrics/RNKissmetrics/RNKissmetrics.m
@@ -10,6 +10,8 @@
 
 #import "RCTLog.h"
 
+#import <KISSmetricsSDK/KISSmetricsAPI.h>
+
 @implementation RNKissmetrics
 
 RCT_EXPORT_MODULE();
@@ -18,5 +20,67 @@ RCT_EXPORT_METHOD(log:(NSString *)message)
 {
     RCTLogInfo(@"RNKissmetricsLog:: %@", message);
 }
+
+RCT_EXPORT_METHOD(configure:(NSDictionary *)config)
+{
+//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    NSString *key = [RCTConvert NSString:config[@"apiKey"]];
+    [KISSmetricsAPI sharedAPIWithKey:key];
+    if ([RCTConvert BOOL:config[@"autoRecordInstalls"]]) {
+        [[KISSmetricsAPI sharedAPI] autoRecordInstalls];
+    }
+    if ([RCTConvert BOOL:config[@"autoSetAppProperties"]]) {
+        [[KISSmetricsAPI sharedAPI] autoSetAppProperties];
+    }
+    if ([RCTConvert BOOL:config[@"autoSetHardwareProperties"]]) {
+        [[KISSmetricsAPI sharedAPI] autoSetHardwareProperties];
+    }
+}
+
+// identify
+RCT_EXPORT_METHOD(identify:(NSString *)identity)
+{
+//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    [[KISSmetricsAPI sharedAPI] identify:identity];
+}
+
+// identity
+RCT_EXPORT_METHOD(identity resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    NSString *currentIdentity = [[KISSmetricsAPI sharedAPI] identity];
+    resolve(currentIdentity);
+}
+
+// clearIdentity
+RCT_EXPORT_METHOD(clearIdentity)
+{
+//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    [[KISSmetricsAPI sharedAPI] clearIdentity]
+}
+
+
+// alias
+RCT_EXPORT_METHOD(alias:(NSString *)alias withIdentity:(NSString *)identity)
+{
+//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    [[KISSmetricsAPI sharedAPI] alias:alias withIdentity:identity];
+}
+
+// record
+RCT_EXPORT_METHOD(record:(NSString *)eventName withProperties:(NSDictionary *)properties)
+{
+//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    [[KISSmetricsAPI sharedAPI] record:eventName withProperties:properties];
+}
+
+
+// set
+RCT_EXPORT_METHOD(set:(NSDictionary *)properties)
+{
+//    RCTLogError(@"RNKissmetrics:: implement me! %@", NSStringFromSelector(_cmd));
+    [[KISSmetricsAPI sharedAPI] set:properties];
+}
+
 
 @end
